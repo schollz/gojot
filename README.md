40ddc4d82031f4ecb086097d8b0faf66376ddbb4
Zack Scholl <zack.scholl@gmail.com>
2016-05-28 08:00:19 -0400

    F

---
+++ README.md
@@ -0,0 +1,7 @@
+# droppybox
+
+## python wrapper for distributed log
+
+```
+sudo python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='droppybox',
+    version='0.1',
+    description='A simple wrapper to produce a GPG-protected, distributed journal log',
+    author='schollz',
+    url='',
+    license='MIT',
+    packages=['droppybox'],
+    entry_points={'console_scripts': ['droppybox = droppybox.droppybox', ], },
+)
