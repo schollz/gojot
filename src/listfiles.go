52098288722259b9a1bfcacfe0849a286839aca1
Zack Scholl <zack.scholl@gmail.com>
2016-09-30 14:00:41 -0400

    List files now occurs in alphabetical order

--- src/listfiles.go
+++ src/listfiles.go
@@ -3,6 +3,7 @@ package sdees
 import (
 	"os"
 	"os/exec"
+	"sort"
 	"strings"
 	"time"
 )
@@ -21,14 +22,24 @@ func ListFiles(gitfolder string) ([]string, []bool) {
 	if err != nil {
 		logger.Error("Problem doing ls-tree")
 	}
-	documents := []string{}
-	encrypted := []bool{}
+	docMap := make(map[string]bool)
 	for _, document := range strings.Split(strings.TrimSpace(string(stdout)), "\n") {
 		if document[0] == '.' {
 			continue
 		}
-		encrypted = append(encrypted, strings.Contains(document, ".gpg"))
-		documents = append(documents, strings.Replace(document, ".gpg", "", -1))
+		docMap[strings.Replace(document, ".gpg", "", -1)] = strings.Contains(document, ".gpg")
+	}
+
+	documents := make([]string, len(docMap))
+	i := 0
+	for k, _ := range docMap {
+		documents[i] = k
+		i++
+	}
+	sort.Strings(documents)
+	encrypted := make([]bool, len(documents))
+	for i, val := range documents {
+		encrypted[i] = docMap[val]
 	}
 	return documents, encrypted
 }
--- src/listfiles_test.go
+++ src/listfiles_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestListFiles(t *testing.T) {
 	files, _ := ListFiles("./gittest10")
-	if (files[0] == "test.txt" || files[0] == "other.txt") && len(files) == 2 {
+	if (files[0] == "other.txt" || files[1] == "test.txt") && len(files) == 2 {
 	} else {
 		t.Errorf("Not correcting listing files: %v", files)
 	}
