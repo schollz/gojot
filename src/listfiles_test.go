c6044e1425df814fa65b3a16483a4ecb59cf0a83
Zack Scholl <zack.scholl@gmail.com>
2016-10-26 13:39:54 -0400

    Fixed List files

--- src/listfiles_test.go
+++ src/listfiles_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestListFiles(t *testing.T) {
 	files := ListFiles("./gittest10")
-	if files[0] == ("test.txt") || files[1] == ("other.txt") {
+	if (files[0] == ("test.txt") && files[1] == ("other.txt")) || (files[1] == ("test.txt") && files[0] == ("other.txt")) {
 	} else {
 		t.Errorf("Not correcting listing files: %v", files)
 	}
