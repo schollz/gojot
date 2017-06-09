91c1a8f1bcc42aa8f30caebf99db0231fe6e7cbb
Zack Scholl <zack.scholl@gmail.com>
2017-06-08 19:12:51 -0600

    Allow entry picking, showing all, better flags, better UI

--- gojot/cli.py
+++ gojot/cli.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import click
-from gojot import gojot
+from gojot import *
 
 
 @click.command()
-@click.argument('repo', nargs=1)
-@click.option('--doc', default=None)
-@click.option('--imp', default=None)
-def main(repo, doc, imp, args=None):
-    """Console script for gojot"""
-    click.echo("Replace this message by putting your code into "
-               "gojot.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.argument('repo')
+@click.option('--doc', '-d', default=None, help='Document to open.')
+@click.option('--imp', '-i', default=None, help='File to import.')
+@click.option('--all', '-a', 'load_all', is_flag=True, help='Open all entries.')
+@click.option('--edit', '-e', 'edit_one', is_flag=True, help='Edit an entry.')
+def main(repo, doc, imp, load_all, edit_one, args=None):
+    """gojot is a modern command-line journal that is distributed and encrypted by default."""
+    click.echo("")
+    click.echo("See gojot documentation at https://gojot.schollz.com/")
     if imp != None:
-        gojot.run_import(repo, imp)
+        run_import(repo, imp)
     else:
-        gojot.run(repo, doc)
+        run(repo, doc, load_all=load_all, edit_one=edit_one)
     # TODO
     # Add flag for editing a single entry (boolean)
 
