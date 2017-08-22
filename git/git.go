6ff7b61403599311ff551641286cc3211c9fd185
Zack Scholl <zack.scholl@gmail.com>
2017-08-22 10:43:33 -0600

    Add cloning

--- git/git.go
+++ git/git.go
@@ -1,19 +1,69 @@
 package git
 
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+)
+
 // GitRepo is the basic store object.
 type GitRepo struct {
 	repo   string
+	root   string
 	folder string
 }
 
 // New returns a new GPGStore that can then needs to be initialized with Init()
-func New(repo, folder string) (*Repo, error) {
+func New(repo, rootDir string) (*GitRepo, error) {
 	gr := new(GitRepo)
 	gr.repo = repo
-	gr.folder = folder
+	gr.root = rootDir
+	gr.folder = parseRepoFolder(repo)
 	return gr, nil
 }
 
 // Update will clone a repo if it doesn't exist or pull a repo, if it does.
-func (gr *GitRepo) Update() error {
+func (gr *GitRepo) Update() (err error) {
+	cwd, err := os.Getwd()
+	defer os.Chdir(cwd)
+	os.Chdir(gr.root)
+	var cmd *exec.Cmd
+	var stdoutStderr []byte
+	if !exists(path.Join(gr.root, gr.folder)) {
+		// git clone repo
+		cmd = exec.Command("git", "clone", gr.repo)
+	} else {
+		// git pull --rebase origin master
+		os.Chdir(gr.folder)
+		cmd = exec.Command("git", "pull", "--rebase", "origin", "master")
+	}
+	stdoutStderr, err = cmd.CombinedOutput()
+	fmt.Printf("%s\n", stdoutStderr)
+	if bytes.Contains(stdoutStderr, []byte("fatal")) {
+		err = errors.New("Could not clone repo")
+	}
+	return
+}
+
+func parseRepoFolder(repo string) (folder string) {
+	firstPart := strings.Split(repo, ".git")[0]
+	firstPartSplit := strings.Split(firstPart, "/")
+	folder = strings.TrimSpace(firstPartSplit[len(firstPartSplit)-1])
+	return
+}
+
+// exists returns whether the given file or directory exists or not
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
--- git/git_test.go
+++ git/git_test.go
@@ -7,6 +7,14 @@ import (
 )
 
 func TestGeneral(t *testing.T) {
-	gr, err := New("repo", "folder")
+	gr, err := New("https://github.com/schollz/schollz/asdf.git", "/tmp/testtest")
 	assert.Equal(t, nil, err)
+	assert.Equal(t, true, nil != gr.Update())
+
+	gr, err = New("https://github.com/schollz/schollz/test.git", "/tmp/testtest")
+	assert.Equal(t, nil, err)
+	assert.Equal(t, nil, gr.Update())
+
+	assert.Equal(t, "test", parseRepoFolder("git@github.com:schollz/test.git"))
+	assert.Equal(t, "gojot", parseRepoFolder("https://github.com/schollz/gojot.git"))
 }
